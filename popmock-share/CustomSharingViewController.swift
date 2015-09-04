1ee4f620e5d54e4a19867847aaa6426928a11b14
syshen <sysheen@gmail.com>
2015-09-04 17:13:54 +0800

    custom sharing viewcontroller

--- popmock-share/CustomSharingViewController.swift
+++ popmock-share/CustomSharingViewController.swift
@@ -1,22 +1,21 @@
 //
-//  ShareViewController.swift
-//  popmock-share
+//  CustomSharingViewController.swift
+//  popmock
 //
-//  Created by syshen on 9/3/15.
+//  Created by syshen on 9/4/15.
 //  Copyright (c) 2015 syshen. All rights reserved.
 //
 
 import UIKit
-import Social
 import MobileCoreServices
 
-class ShareViewController: SLComposeServiceViewController {
+class CustomSharingViewController: UITableViewController {
 
     var selectedImage:UIImage?
-    
+
     override func viewDidLoad() {
-        
         super.viewDidLoad()
+
         
         let content = self.extensionContext!.inputItems[0] as! NSExtensionItem
         let contentType = kUTTypeImage as String
@@ -45,15 +44,15 @@ class ShareViewController: SLComposeServiceViewController {
             }
         }
         
-    }
-    override func isContentValid() -> Bool {
-        if self.selectedImage != nil {
-            return true
-        }
-        
-        return false
+        let barItem = UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.Done, target: self, action: "cancel:")
+        self.navigationItem.rightBarButtonItem = barItem
+
     }
     
+    func cancel(sender:AnyObject) {
+        self.extensionContext?.completeRequestReturningItems(nil, completionHandler: nil)
+    }
+
     func uniqueFileName() -> String {
         let now = NSDate()
         let dateFormatter = NSDateFormatter()
@@ -61,54 +60,43 @@ class ShareViewController: SLComposeServiceViewController {
         return dateFormatter.stringFromDate(now)
     }
 
-    override func didSelectPost() {
-        
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("projectCell", forIndexPath: indexPath) as! UITableViewCell
+
+        cell.textLabel?.text = "Project #\(indexPath.row)"
+
+        return cell
+    }
+
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         if let docUrl = NSFileManager.defaultManager().containerURLForSecurityApplicationGroupIdentifier("group.in.popapp") {
             if let image = self.selectedImage {
                 let url = docUrl.URLByAppendingPathComponent(self.uniqueFileName()).URLByAppendingPathExtension("png")
                 let data = UIImagePNGRepresentation(image)
                 data.writeToURL(url, atomically: true)
+                
+                self.extensionContext?.completeRequestReturningItems(nil, completionHandler: nil)
+
             }
         }
-        
-        self.extensionContext!.completeRequestReturningItems([], completionHandler: nil)
     }
 
-    override func configurationItems() -> [AnyObject]! {
 
-        
-        let item1 = SLComposeSheetConfigurationItem()
-        item1.title = "Projects"
-        item1.value = "test project"
-        
-        item1.tapHandler = { [unowned self] in
-            let projectsViewController = ProjectsViewController()
-            self.pushConfigurationViewController(projectsViewController)
-        }
-        
-        return [item1]
-    }
-
-}
-
-class ProjectsViewController: UITableViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.tableView.alpha = 0.5
-        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "projectCell")
-    }
-    
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
-    }
-    
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("projectCell", forIndexPath: indexPath) as! UITableViewCell
-        cell.textLabel?.text = "Project \(indexPath.row)"
-        return cell
-    }
 }
